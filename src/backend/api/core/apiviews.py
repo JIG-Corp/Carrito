2f0c575d54909d38ee1cbf54647e2594cd14ca41
TheMegax <mathias.tello12@gmail.com>
2024-07-07 14:53:09 -0400

    Update frontend

--- src/backend/api/core/apiviews.py
+++ src/backend/api/core/apiviews.py
@@ -1,7 +1,7 @@
-from tokenize import Token 
-from rest_framework import status 
-from rest_framework.views import APIView 
-from rest_framework.response import Response 
+from tokenize import Token
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from apps.core import utils
 
 class Persona(APIView):
--- src/frontend/src/app/pages/ui-components/register-prod/register-prod.component.ts
+++ src/frontend/src/app/pages/ui-components/register-prod/register-prod.component.ts
@@ -8,6 +8,7 @@ import {MatInput} from "@angular/material/input";
 import {RouterLink} from "@angular/router";
 import {MatOption, MatSelect} from "@angular/material/select";
 import {CurrencyPipe} from "@angular/common";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-register-prod',
@@ -29,5 +30,111 @@ import {CurrencyPipe} from "@angular/common";
   templateUrl: './register-prod.component.html',
   styleUrl: './register-prod.component.scss'
 })
-export class RegisterProdComponent {}
+export class RegisterProdComponent {
+    async addProduct() {
+    type AddProductForm = {
+      nombre: string;
+      descripcion: string;
+      categoria: string;
+      precio: number;
+    };
+
+    let nombreInput: HTMLInputElement;
+    let descripcionInput: HTMLInputElement;
+    let categoriaInput: HTMLInputElement;
+    let precioInput: HTMLInputElement;
+
+    Swal.fire<AddProductForm>({
+      title: "Agregar producto",
+      html: `
+    <mat-label class="mat-subtitle-1 f-w-600 d-block">
+        Nombre del Producto
+    </mat-label>
+    <input type="text" id="nombre" class="swal2-input">
+    <mat-label class="mat-subtitle-1 f-w-600 d-block">
+        Descripción
+    </mat-label>
+    <input type="text" id="descripcion" class="swal2-input">
+    <mat-label class="mat-subtitle-1 f-w-600 d-block">
+        Categoría
+    </mat-label>
+    <input type="text" id="categoria" class="swal2-input">
+    <mat-label class="mat-subtitle-1 f-w-600 d-block">
+        Precio (CLP$)
+    </mat-label>
+    <input type="text" id="precio" class="swal2-input">
+    `,
+      confirmButtonText: "Guardar",
+      focusConfirm: false,
+      didOpen: () => {
+        const popup = Swal.getPopup()!;
+        nombreInput = popup.querySelector("#nombre") as HTMLInputElement;
+        descripcionInput = popup.querySelector("#descripcion") as HTMLInputElement;
+        categoriaInput = popup.querySelector("#categoria") as HTMLInputElement;
+        precioInput = popup.querySelector("#precio") as HTMLInputElement;
+
+        nombreInput.onkeyup = (event) =>
+          event.key === "Enter" && Swal.clickConfirm();
+        descripcionInput.onkeyup = (event) =>
+          event.key === "Enter" && Swal.clickConfirm();
+        categoriaInput.onkeyup = (event) =>
+          event.key === "Enter" && Swal.clickConfirm();
+        precioInput.onkeyup = (event) =>
+          event.key === "Enter" && Swal.clickConfirm();
+      },
+      preConfirm: () => {
+        const nombre = nombreInput.value;
+        const descripcion = descripcionInput.value;
+        const categoria = categoriaInput.value;
+        const precio = precioInput.value;
+        if (!nombre || !descripcion || !categoria || !precio)  {
+          Swal.showValidationMessage(`Por favor rellene los campos`);
+        }
+        return { nombre, descripcion, categoria, precio };
+      },
+    }).then(async (result) => {
+      if(result.value){
+
+        const response = await fetch(
+          `http://localhost:8000/api/core/post/Persona/`,
+          {
+            method: "POST",
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              nombre: nombreInput.value,
+              descripcion: descripcionInput.value,
+              categoria: categoriaInput.value,
+              precio : precioInput.value
+            })
+          }
+        );
+
+        if (response.status >= 200 && response.status <= 205) {
+          Swal.fire({
+            title: "Creado",
+            text: `${nombreInput.value} se a creado de los registros`,
+            icon: "success",
+          }).then((ok) => {
+            if (ok.value) {
+              this.getPeople();
+            }
+          });
+        } else {
+          Swal.fire({
+            title: "Error",
+            text: `No se pudo crear a ${nombreInput.value}`,
+            icon: "error",
+          });
+        }
+      }
+    });
+  }
+
+    private getPeople() {
+
+    }
+}
 
